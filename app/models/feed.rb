f802ba56289c0d040717bc8ff494e7d0c13d88e4
Alex Wayne <alex@beautifulpixel.com>
2008-08-02 00:22:03 -0700

    Added a story_id to the story feed.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,5 @@
 class Feed
-  attr_accessor :posts
+  attr_accessor :posts, :story_id
   
   def initialize(options = {})
     if options[:story_id]
@@ -12,6 +12,11 @@ class Feed
     
     page = Hpricot(open(url))
     @posts = []
+    
+    @story_id = options[:story_id]
+    if (page / 'div.story h1 a').any?
+      @story_id = (page / 'div.story h1 a').first[:href].gsub('/onearticle.x/', '')
+    end
     
     (page / 'div.root').each do |xml|
       @posts << Post.new(xml, :parent => nil, :parse_children => options[:parse_children])
--- app/views/parse/index.xml.builder
+++ app/views/parse/index.xml.builder
@@ -1,6 +1,6 @@
 xml.instruct!
-xml.comments {
+xml.comments :story_id => @feed.story_id do
   @feed.posts.each do |post|
     xml << render(:partial => 'comment', :locals => { :comment => post, :xml => xml })
   end
-}
+end

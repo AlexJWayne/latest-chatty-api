5187219bef7095f38abcde630b4231a13c7b4293
Alex Wayne <alex@beautifulpixel.com>
2008-08-02 09:52:18 -0700

    Temporarily disable password logging

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,14 +4,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  # protect_from_forgery # :secret => 'a919cf30ec493451f803d67a7015317e'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  filter_parameter_logging :password
+  # filter_parameter_logging :password
   
   session :off
 end

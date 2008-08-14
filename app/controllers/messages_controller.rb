eef74973603e84f72c2504cff37d2c2a99be62a4
Alex Wayne <alex@beautifulpixel.com>
2008-08-14 12:24:10 -0700

    cleaned up authentication

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,21 @@
 class MessagesController < ApplicationController
+  before_filter :auth
   
   def index
-    authenticate_or_request_with_http_basic do |username, password|
-      @username = username
-      @messages = Message.fetch(username, password)
-      if @messages == :not_authorized
-        render :text => '<error>Authentication Failed</error>', :status => response.status
-      else
-        true
-      end
-    end
+    @messages = Message.fetch(@username, @password)
   end
   
   def read
-    authenticate_or_request_with_http_basic do |username, password|
-      response = Message.read(username, password, params[:id])
-      if @messages == :not_authorized
-        render :text => '<error>Authentication Failed</error>', :status => response.status
-      else
-        render :text => '<error>Success</error>'
+    @response = Message.read(@username, @password, params[:id])
+  end
+  
+  protected
+    def auth
+      authenticate_or_request_with_http_basic do |username, password|
+        @username = username
+        @password = password
+        LoginCookie.new(username, password).success?
       end
     end
-  end
     
 end

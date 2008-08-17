51d4f6058f16c18fcfb368d9a9373bf4a76b7dc5
Alex Wayne <alex@beautifulpixel.com>
2008-08-17 00:40:59 -0700

    User authentication is now required to post pics.

--- app/models/login_cookie.rb
+++ app/models/login_cookie.rb
@@ -2,7 +2,6 @@ class LoginCookie
   attr_reader :string, :status
   
   def initialize(username, password)
-    # Post the login
     response = Net::HTTP.post_form(URI.parse('http://www.shacknews.com/login_laryn.x'), {
       :username => username,
       :password => password,
@@ -21,5 +20,24 @@ class LoginCookie
   
   def success?
     @status == :success
+  end
+  
+  
+  
+  class ShackPics < LoginCookie
+    def initialize(username, password)
+      
+      response = `curl -i -d "user_name=#{username}&user_password=#{password}" http://www.shackpics.com/users.x?act=login_go`
+      puts response
+      
+      if cookie = response.split("\n").find { |line| line =~ /^Set-Cookie: mmh_user_cookie=(.+?\.[A-Za-z0-9\.]+?);/ }
+        cookie_value = $1
+        
+        @string = "mmh_user_cookie=#{cookie_value}"
+        @status = :success
+      else
+        @status = :not_authorized
+      end
+    end
   end
 end

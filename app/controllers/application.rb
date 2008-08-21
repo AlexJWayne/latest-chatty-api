9fab0266112fe7fbd22ba5f0da6cda43968aeb62
Alex Wayne <alex@beautifulpixel.com>
2008-08-21 16:19:30 -0700

    remove exception notifier

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,7 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include ExceptionNotifiable
   helper :all # include all helpers, all the time
   filter_parameter_logging :password, :image
   session :off

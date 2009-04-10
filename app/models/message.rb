94dffde8cd12c8021387c7fd1264725e455ba57a
Alex Wayne <alex@beautifulpixel.com>
2009-04-10 14:44:14 -0700

    Trim message bodies better.

--- app/models/message.rb
+++ app/models/message.rb
@@ -23,7 +23,7 @@ class Message
             :subject  => row.find_first('.//td[contains(@class, "subject")]//a').content,
             :unread   => row.find_first('.//td[contains(@class, "subject")]')[:class].include?('unread'),
             :date     => row.find_first('.//td[contains(@class, "date")]').content,
-            :body     => page.find_first("//tr[@id='msgview_#{id}']//div[@id='msgcopy']").to_s.inner_html.strip,
+            :body     => page.find_first("//tr[@id='msgview_#{id}']//div[@id='msgcopy']").to_s.inner_html.gsub(/^&#13;|&#13;$/, '').strip,
           })
         end
       end
